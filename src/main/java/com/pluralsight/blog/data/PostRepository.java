0241eddfdef49eb72c7823e638bb52c8085b6d55
Miguel Quintero <migmaqer@gmail.com>
2020-07-13 20:06:13 -0400

    add postrepository
    Signed-off-by: Miguel Quintero <migmaqer@gmail.com>

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -4,5 +4,9 @@ import com.pluralsight.blog.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
-public interface PostRepository extends JpaRepository<Post, Long> {}
+public interface PostRepository extends JpaRepository<Post, Long> {
+    public List<Post> findByTitleContaining(String title);
+}
