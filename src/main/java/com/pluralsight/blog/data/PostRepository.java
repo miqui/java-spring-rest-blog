f034eaca359512a40f91015407bb8a97fcf665ac
Miguel Quintero <migmaqer@gmail.com>
2020-07-13 20:11:09 -0400

    add @restresource
    Signed-off-by: Miguel Quintero <migmaqer@gmail.com>

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -2,11 +2,14 @@ package com.pluralsight.blog.data;
 
 import com.pluralsight.blog.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    @RestResource(rel="constains-title", path="constainsTitle")
     public List<Post> findByTitleContaining(String title);
 }
